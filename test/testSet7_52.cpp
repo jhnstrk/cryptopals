04f20e7152b2bdfddce84e78e1e1afa44b10b2d7
John Stark <jhnstrk@gmail.com>
2017-09-18 23:07:18 +0200

    Refactoring the Challenge 52 set a bit.

--- test/testSet7_52.cpp
+++ test/testSet7_52.cpp
@@ -1,5 +1,6 @@
 #include "testSet7_52.h"
 
+#include <bitsnbytes.h>
 #include <utils.h>
 
 #include <QDebug>
@@ -10,71 +11,151 @@
 JDS_ADD_TEST(TestSet7_52)
 
 namespace {
-    const quint16 H0 = 0x3141;
+    QByteArray H0() {
+        QByteArray ret;
+        ret.resize(2);
+        ret[0] = (char)0x31;
+        ret[1] = (char)0x41;
+        return ret;
+    }
 
-    // f
-    class MdHash16 {
-    public:
-        static quint16 hash(const QByteArray & data, quint16 h0 = H0)
+    class MdHashBase {
+    protected:
+        MdHashBase() : m_count(0) {}
+        virtual ~MdHashBase() {}
+
+        static const int BlockSizeBytes = qossl::AesBlockSize;
+
+        QByteArray finalize()
         {
-            const int AesBlockSize = qossl::AesBlockSize;
-            QByteArray padKey = QByteArray(AesBlockSize, (char)0);
-
-            quint16 h = h0;  // Fixed initial value: Digits of Pi.
-
-            for (int i = 0; i < data.size(); i += AesBlockSize)
-            {
-                padKey[0] = (char)( h &0xFF);
-                padKey[1] = (char)((h >> 8)&0xFF);
-                QByteArray nextBlock = data.mid(i, AesBlockSize);
-
-                if (nextBlock.size() < AesBlockSize) {
-                    // Only last block. Pad with zero.
-                    nextBlock += QByteArray(AesBlockSize- nextBlock.size(), (char)0);
-                }
-
-                QByteArray block = qossl::aesEcbEncrypt(nextBlock, padKey);
-
-                h = (unsigned)(unsigned char)(block.at(0))
-                        | ((unsigned)(unsigned char)(block.at(1)) << 8);
-
+            m_buffer.append((char)0x80);
+            if (m_buffer.size() <= (BlockSizeBytes - 8) ) {
+                m_buffer.append( QByteArray((BlockSizeBytes - 8) - m_buffer.size(),'\0'));
+            } else {
+                m_buffer.append( QByteArray(BlockSizeBytes - m_buffer.size(),'\0'));
+                this->addBlock(m_buffer,0);
+                m_buffer = QByteArray((BlockSizeBytes - 8),'\0');
             }
 
-            return h;
+            // Message length in bits
+            m_buffer.append( qossl::uint64Le(m_count * CHAR_BIT) );
+
+            this->addBlock(m_buffer,0);
+
+            return m_state;
         }
-        private:
+
+        void addData(const QByteArray & data)
+        {
+            m_count += data.size();
+
+            // less then a block
+            if (m_buffer.size() + data.size() < BlockSizeBytes) {
+                m_buffer.append(data);
+                return;
+            }
+
+            int dataIndex = 0;
+            if (!m_buffer.isEmpty()) {
+                // fill buffer
+                dataIndex = BlockSizeBytes - m_buffer.size();
+                m_buffer.append(data.mid(0,dataIndex));
+                this->addBlock(m_buffer,0);
+            }
+
+            int end = dataIndex + BlockSizeBytes;
+            while (end <= data.size()) {
+                this->addBlock(data, dataIndex);
+                dataIndex = end;
+                end += BlockSizeBytes;
+            }
+
+            m_buffer = data.mid(dataIndex);
+        }
+
+        virtual void addBlock(const QByteArray &data, int offset) = 0;
+    protected:
+        QByteArray m_state;
+        int m_count;
+    private:
+        QByteArray m_buffer;
+    };
+
+    // f
+    class MdHash16 : public MdHashBase {
+    public:
+        MdHash16() {
+            this->reset();
+        }
+
+        void reset() {
+            m_state = H0();
+            m_count = 0;
+        }
+
+        static QByteArray hash(const QByteArray & data)
+        {
+            MdHash16 hasher;
+            hasher.addData(data);
+            return hasher.finalize();
+        }
+
+        void addBlock(const QByteArray &data, int offset) Q_DECL_OVERRIDE
+        {
+            this->m_state = MdHash16::iteration(data.mid(offset,BlockSizeBytes), this->m_state);
+        }
+
+        static QByteArray iteration(const QByteArray & block, const QByteArray & hIn)
+        {
+            QByteArray padKey = QByteArray(BlockSizeBytes, (char)0);
+            padKey[0] = hIn.at(0);
+            padKey[1] = hIn.at(1);
+
+            QByteArray eblock = qossl::aesEcbEncrypt(block, padKey);
+            eblock.resize(2);
+            return eblock;
+        }
+
     };
 
 
     // g : 24-bit version.
-    class MdHash24 {
+    class MdHash24 : public MdHashBase {
     public:
-        static quint32 hash(const QByteArray & data, quint32 h0 = 0x314159)
+        MdHash24() {
+            this->reset();
+        }
+
+        void reset() {
+            m_state.resize(3);
+            m_state[0] = (char)0x31;
+            m_state[1] = (char)0x41;
+            m_state[2] = (char)0x59;  // Digits of Pi.
+            m_count = 0;
+        }
+
+        static QByteArray hash(const QByteArray & data)
         {
-            const int AesBlockSize = qossl::AesBlockSize;
-            QByteArray padKey = QByteArray(AesBlockSize, (char)0);
+            MdHash24 hasher;
+            hasher.addData(data);
+            return hasher.finalize();
+        }
 
-            quint32 h = h0;  // Fixed initial value: Digits of Pi.
+        void addBlock(const QByteArray &data, int offset) Q_DECL_OVERRIDE
+        {
+            this->m_state = MdHash16::iteration( data.mid(offset,BlockSizeBytes), this->m_state);
+        }
 
-            for (int i = 0; i < data.size(); i += AesBlockSize)
-            {
-                padKey[0] = (char)( h &0xFF);
-                padKey[1] = (char)((h >> 8)&0xFF);
-                padKey[2] = (char)((h >>16)&0xFF);
-                QByteArray nextBlock = data.mid(i, AesBlockSize);
+        static QByteArray iteration(const QByteArray & block, const QByteArray & hIn)
+        {
+            QByteArray padKey = QByteArray(BlockSizeBytes, (char)0);
+            padKey[0] = hIn.at(0);
+            padKey[1] = hIn.at(1);
+            padKey[2] = hIn.at(2);
 
-                if (nextBlock.size() < AesBlockSize) {
-                    // Only last block. Pad with zero.
-                    nextBlock += QByteArray(AesBlockSize - nextBlock.size(), (char)0);
-                }
-
-                QByteArray block = qossl::aesEcbEncrypt(nextBlock, padKey);
-                h = (unsigned)(unsigned char)(block.at(0))
-                        | ((unsigned)(unsigned char)(block.at(1)) << 8)
-                        | ((unsigned)(unsigned char)(block.at(2)) << 16);
-            }
-
-            return h;
+            QByteArray eblock = qossl::aesEcbEncrypt(block, padKey);
+            eblock.resize(3);
+            return eblock;
         }
         private:
     };
@@ -87,43 +168,44 @@ namespace {
 
     struct Collision {
         Collision() {}
-        Collision( quint16 hi, quint16 ho, const QByteArray & col1,const QByteArray & col2 ):
-            hIn(hi),
-            hOut(ho),
+        Collision( const QByteArray& hi, const QByteArray & ho,
+                   const QByteArray & col1,const QByteArray & col2 ):
+            hIn(hi), // Input state
+            hOut(ho), // output state
             c1(col1),
             c2(col2)
         {}
 
-        quint16 hIn;
-        quint16 hOut;
+        QByteArray hIn;
+        QByteArray hOut;
         QByteArray c1;
         QByteArray c2;
     };
 
-    Collision findCollision(const quint16 hIn)
+    Collision findCollision(const QByteArray& hIn)
     {
         ++m_cfCount;
 
-        QHash< quint16 ,QByteArray > tested;
+        QHash< QByteArray ,QByteArray > tested;
         while(true) {
             QByteArray sample = qossl::randomBytes(qossl::AesBlockSize);
 
-            quint16 hOut = MdHash16::hash(sample,hIn);
+            QByteArray hOut = MdHash16::iteration(sample,hIn);
             if (tested.contains(hOut)) {
                 if (tested.value(hOut) != sample) {
                     return Collision(hIn, hOut, tested.value(hOut), sample );
                 }
             } else {
-                tested[hOut].append(sample);
+                tested[hOut] = sample;
             }
         }
     }
 
     void find2nCollisions(int n)
     {
-        quint16 h;
+        QByteArray h;
         if (m_collisions.isEmpty()) {
-            h = H0;
+            h = H0();
         } else {
             h = m_collisions.back().hOut;
         }
@@ -132,14 +214,15 @@ namespace {
             Collision nextC = findCollision(h);
             m_collisions.append(nextC);
             h = nextC.hOut;
-            qDebug() << "Collision" << nextC.hIn << nextC.hOut << nextC.c1.toHex() << nextC.c2.toHex();
+            qDebug() << "Collision" << nextC.hIn.toHex() << nextC.hOut.toHex()
+                     << nextC.c1.toHex() << nextC.c2.toHex();
         }
         return;
     }
 
-    quint16 hOut(int nMax = -1) const {
+    QByteArray hOut(int nMax = -1) const {
         if (m_collisions.isEmpty()) {
-            return H0;
+            return H0();
         }
         if (nMax == -1) {
             nMax = m_collisions.size() - 1;
@@ -186,12 +269,20 @@ void TestSet7_52::testChallenge52()
     cf.find2nCollisions(n);
 
     // print all 2^4 collisions.
+    QByteArray cValue;
     for (unsigned int i=0; i< (1<<n); ++i)
     {
-        QByteArray data = cf.makeCollision(i);
-        quint16 h = MdHash16::hash(data);
-        QCOMPARE(h, cf.hOut());
+        const QByteArray data = cf.makeCollision(i);
+        const QByteArray h = MdHash16::hash(data);
+        qDebug() << i << h.toHex();
+        if (i == 0) {
+            cValue = h;
+        } else {
+            QCOMPARE(h, cValue);  // Ensure collision, including padding matches.
+        }
     }
+
+    qDebug() << "Found 2^4 collisions";
 
     // Keep increasing the number of messages in the pool until we hit a collision.
     bool haveCollision =false;
@@ -199,16 +290,15 @@ void TestSet7_52::testChallenge52()
 
         // Find more 2^4 times more collisions.
         cf.find2nCollisions(4);
-        QHash < quint32, unsigned int > gHashes;
+        QHash < QByteArray, unsigned int > gHashes;
 
         n = cf.nMax();
-        qDebug() << n;
 
         // See if there's a match in g.
         for (unsigned int i=0; i< (1<<n); ++i)
         {
             QByteArray data = cf.makeCollision(i);
-            quint32 h = MdHash24::hash(data);
+            QByteArray h = MdHash24::hash(data);
             if (gHashes.contains(h)) {
                 qDebug() << "Collision in g found";
                 haveCollision = true;
